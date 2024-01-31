8a00a1cc13b6d62cf1e893676f12d2c852833fea
Its-Alamin-H <alaminhosssain97@gmail.com>
2024-01-31 19:09:19 +0600

    fixeed

--- components/main/Navbar.tsx
+++ components/main/Navbar.tsx
@@ -1,60 +1,3 @@
-// import { Socials } from "@/constants";
-// import Image from "next/image";
-// import React from "react";
-
-// const Navbar = () => {
-//   return (
-//     <div className="w-full h-[65px] fixed top-0 shadow-lg shadow-[#2A0E61]/50 bg-[#03001417] backdrop-blur-md z-50 px-10">
-//       <div className="w-full h-full flex flex-row items-center justify-between m-auto px-[10px]">
-//         <a
-//           href="#about-me"
-//           className="h-auto w-auto flex flex-row items-center"
-//         >
-//           <Image
-//             src="/NavLogo.png"
-//             alt="logo"
-//             width={70}
-//             height={70}
-//             className="cursor-pointer hover:animate-slowspin"
-//           />
-
-//           <span className="font-bold ml-[10px] hidden md:block text-gray-300">
-//             WebChain Dev
-//           </span>
-//         </a>
-
-//         <div className="w-[500px] h-full flex flex-row items-center justify-between md:mr-20">
-//           <div className="flex items-center justify-between w-full h-auto border border-[#7042f861] bg-[#0300145e] mr-[15px] px-[20px] py-[10px] rounded-full text-gray-200">
-//             <a href="#about-me" className="cursor-pointer">
-//               About me
-//             </a>
-//             <a href="#skills" className="cursor-pointer">
-//               Skills
-//             </a>
-//             <a href="#projects" className="cursor-pointer">
-//               Projects
-//             </a>
-//           </div>
-//         </div>
-
-//         <div className="flex flex-row gap-5">
-//           {Socials.map((social) => (
-//             <Image
-//               src={social.src}
-//               alt={social.name}
-//               key={social.name}
-//               width={24}
-//               height={24}
-//             />
-//           ))}
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Navbar;
-
 "use client"; // This is a client component 👈🏽
 import React, { useState } from "react";
 import Image from "next/image";
@@ -90,30 +33,6 @@ const Navbar = () => {
           >
             Contact Us
           </button>
-          {/* <button
-            onClick={toggleMenu}
-            type="button"
-            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-            aria-controls="navbar-sticky"
-            aria-expanded="false"
-          >
-            <span className="sr-only">Open main menu</span>
-            <svg
-              className="w-5 h-5"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 17 14"
-            >
-              <path
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M1 1h15M1 7h15M1 13h15"
-              />
-            </svg>
-          </button> */}
         </div>
         <div
           className={`items-center justify-between w-full md:flex md:w-auto md:order-1 ${
